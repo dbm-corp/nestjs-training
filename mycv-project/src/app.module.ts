211c4e24cdaa3b2cac4a1fedbcb6e5b44c325c61
binhth <binhth@hocmai.vn>
2023-02-24 17:01:16 +0700

    feat: day 7 part 1 create route create, find, update, delete user and post

--- mycv-project/src/app.module.ts
+++ mycv-project/src/app.module.ts
@@ -4,19 +4,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { PostsModule } from './posts/posts.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { Post } from './posts/post.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User, Report],
+      entities: [User, Report, Post],
       synchronize: true,
     }),
     UsersModule,
     ReportsModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- mycv-project/src/users/users.controller.ts
+++ mycv-project/src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -8,5 +9,29 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      return new NotFoundException('user not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAddUser(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  deletePost(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
