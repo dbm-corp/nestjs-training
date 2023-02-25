72adf632b1c005a786fe771a1892b6ca3eed3dee
binhth <binhth@hocmai.vn>
2023-02-25 10:54:51 +0700

    feat: nestjs custom data serialization to format return data properties

---
+++ mycv-project/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,38 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto';
+
+interface ClassConstructor {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // Run something before a request is handled
+    // by the request handler
+    console.log('Im running before the handler', context);
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        console.log('Im running before response is sent out', data);
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        })
+      })
+    )
+  }
+}
